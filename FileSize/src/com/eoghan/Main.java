cecb8891469af2d892be634d01dc19f15cf02f8d
Eoghan <eoghankenny95@gmail.com>
2019-06-19 05:15:36 +0100

    Add files via upload

---
+++ FileSize/src/com/eoghan/Main.java
@@ -0,0 +1,13 @@
+package com.eoghan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        Solution solution = new Solution();
+//        System.out.println(solution.solution("my.song.mp3 11b\ngreatSong.flac 1000b\nnot3.txt 5b\nvideo.mp4 200b\ngame.exe 100b\nmov!e.mkv 10000b"));
+
+        Phone phone = new Phone();
+        phone.phone("00:01:07,400-234-090\\n00:05:01,701-080-080\\n00:05:00,400-234-090");
+    }
+}
